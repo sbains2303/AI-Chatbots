cad1356a64ec49d3d88b40ed3fbd2f3f8f8e23f6
Saihajpreet Bains <ia22870@bristol.ac.uk>
2023-11-15 16:09:33 +0000

    Navbar update
    Changed navbar style and added website name

--- code/website/app.py
+++ code/website/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
@@ -9,3 +9,11 @@ def index():
 @app.route('/chicken-korma')
 def chickenKorma():
     return render_template("chicken-korma.html")
+
+@app.route('/scrambled-eggs')
+def scrambledEggs():
+    return render_template("scrambled-eggs.html")
+
+@app.route('/salmon-fishcakes')
+def salmonFishcakes():
+    return render_template("salmon-fishcakes.html")
