a638a6906a3cfb8de39895d376225302b9ca52dd
Saihajpreet Bains <ia22870@bristol.ac.uk>
2023-11-21 12:27:41 +0000

    Recipe pages added
    Added new page for users to add new recipes in form and created base jinja2 template for recipe pages as well as example recipes.

--- code/website/app.py
+++ code/website/app.py
@@ -1,20 +1,33 @@
 from flask import Flask, render_template
+import yaml
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("layout.html")
 
-@app.route('/chicken-korma')
-def chickenKorma():
-    return render_template("chicken-korma.html")
+with open("templates/scrambled-eggs-content.yaml", "r") as file:
+    scrambled_eggs_content = yaml.safe_load(file)
 
 @app.route('/scrambled-eggs')
 def scrambledEggs():
-    return render_template("scrambled-eggs.html")
+    return render_template("recipes.html", **scrambled_eggs_content)
+
+with open("templates/chicken-korma.yaml", "r") as file:
+    chicken_korma_content = yaml.safe_load(file)
+
+@app.route('/chicken-korma')
+def chickenKorma():
+    return render_template("recipes.html", **chicken_korma_content)
+
+with open("templates/salmon-fishcakes-content.yaml", "r") as file:
+    salmon_fishcakes_content = yaml.safe_load(file)
 
 @app.route('/salmon-fishcakes')
 def salmonFishcakes():
-    return render_template("salmon-fishcakes.html")
+    return render_template("recipes.html", **salmon_fishcakes_content)
 
+@app.route('/new-recipe')
+def newRecipe():
+    return render_template("new-recipe.html")
