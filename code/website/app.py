08b619b00c1c51c215da9646876e8c01554141af
Emir Abou Zaki <emirabouzaki@gmail.com>
2023-11-21 14:52:54 +0000

    Replaced sqlite code with sqlalchemy

--- code/website/app.py
+++ code/website/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, g
-import sqlite3
+# import sqlite3
+
+import sqlalchemy
 
 app = Flask(__name__, static_url_path='/static')
-app.config['DATABASE'] = 'cookbook.db'
+db = sqlalchemy(app)
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(app.config['DATABASE'])
-    return db
+app.config['DATABASE'] = 'cookbook.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cookbook.db'
+
 
 @app.route('/')
 def index():
@@ -20,67 +20,56 @@ def serveRecipe(recipe_name):
     return render_template("build_recipe.html")
 
 
-@app.route('/chicken-korma')
-def chickenKorma():
-    return render_template("chicken-korma.html")
-  
-@app.route('/scrambled-eggs')
-def scrambledEggs():
-    return render_template("scrambled-eggs.html")
-
-@app.route('/salmon-fishcakes')
-def salmonFishcakes():
-    return render_template("salmon-fishcakes.html")
-
 @app.route('/publish', methods=['POST'])
 def publish():
-    cur = get_db().cursor()
-    authorName = request.form['authorname']
+    author_name = request.form['authorname']
     email = request.form['email']
-    recipeName = request.form['recipename']
-    recipeDesc = request.form['rescipedesc']
+    recipe_name = request.form['recipename']
+    recipe_desc = request.form['recipedesc']
     serves = request.form['serves']
     image = request.form['image']
     time = request.form['time']
-    ingredients = request.form['ingredients'] #list of pairs(ingredient, quantity)
-    steps = request.form['steps'] # list of steps in order
-    cuisine = request.form['cuisine']
+    ingredients = eval(request.form['ingredients'])  # Assuming the ingredients are a list of pairs
+    steps = eval(request.form['steps'])  # Assuming the steps are a list
+    cuisine_name = request.form['cuisine']
 
-    #Populate Cuisines Table
-    cuisineList = cur.execute('''SELECT name FROM Cuisines;''').fetchall() #Creates a list of all cuisines already in the database
-    if cuisine not in cuisineList:
-        cur.execute('''INSERT INTO Cuisines (cuisine_name) VALUES (?);''', (cuisine))
+    # Check if the cuisine exists, if not, add it
+    cuisine = db.Cuisine.query.filter_by(cuisine_name=cuisine_name).first()
+    if not cuisine:
+        cuisine = db.Cuisine(cuisine_name=cuisine_name)
+        db.session.add(cuisine)
+        db.session.commit()
 
-    cuisineID = cur.execute('''SELECT cuisine_id FROM Cuisines WHERE cuisine_name = ?;''', (cuisine)).fetchone()[0]
+    # Check if the author exists, if not, add them
+    author = db.Author.query.filter_by(name=author_name).first()
+    if not author:
+        author = db.Author(name=author_name, email=email)
+        db.session.add(author)
+        db.session.commit()
 
-    #Populate Authors Table
-    cur.execute('''INSERT INTO Authors (name, email) 
-                VALUES (?, ?);''', (authorName, email))
+    # Create the recipe
+    recipe = db.Recipe(
+        name=recipe_name,
+        description=recipe_desc,
+        cuisine=cuisine,
+        serves=serves,
+        image=image,
+        cooking_time=time,
+        author=author
+    )
+    db.session.add(recipe)
+    db.session.commit()
 
-    authorID = cur.execute('''SELECT author_id FROM Authors WHERE name=?;''', (authorName)).fetchone()[0]
-
-    #Populate Recipes Table
-    cur.execute('''INSERT INTO Recipes (name, description, cuisine_id, serves, image, cooking_time, author_id) 
-                VALUES (?, ?, ?, ?, ?, ?, ?);''', (recipeName, recipeDesc,cuisineID, serves, image, time, authorID))
-
-    recipeID =  cur.execute('''SELECT recipe_id FROM Recipes WHERE name=?;''', (recipeName)).fetchone()[0]
-
-    #Populate Ingredients Table
+    # Add ingredients to the recipe
     for ingredient in ingredients:
-        cur.execute('''INSERT INTO Ingredients (recipe_id, name, quantity) 
-                    VALUES (?, ?, ?);''', (recipeID, ingredient[0], ingredient[1]))
+        ingredient_obj = db.Ingredient(recipe=recipe, name=ingredient[0], quantity=ingredient[1])
+        db.session.add(ingredient_obj)
 
-    #Populate Steps Table
-    i=1
-    for step in steps:
-        cur.execute('''INSERT INTO Steps (recipe_id, description, order_number) 
-                    VALUES (?, ?, ?);''', (recipeID,step, i))
-        i+=1
+    # Add steps to the recipe
+    for i, step in enumerate(steps, start=1):
+        step_obj = db.Step(recipe=recipe, description=step, order_number=i)
+        db.session.add(step_obj)
+
+    db.session.commit()
 
     return 'Recipe inserted successfully'
-
-@app.teardown_appcontext
-def close_db(error):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
---
+++ code/website/models.py
@@ -0,0 +1,42 @@
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+from app import app, db
+
+class Author(db.Model):
+    author_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255))
+
+class Cuisine(db.Model):
+    cuisine_id = db.Column(db.Integer, primary_key=True)
+    cuisine_name = db.Column(db.String(255))
+
+class Recipe(db.Model):
+    recipe_id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    cuisine_id = db.Column(db.Integer, db.ForeignKey('cuisine.cuisine_id'))
+    serves = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.LargeBinary, nullable=False)
+    cooking_time = db.Column(db.Integer, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.author_id'))
+    cuisine = db.relationship('Cuisine', backref='recipes')
+    author = db.relationship('Author', backref='recipes')
+
+class Ingredient(db.Model):
+    ingredient_id = db.Column(db.Integer, primary_key=True, unique=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.recipe_id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    quantity = db.Column(db.String(255), nullable=False)
+    recipe = db.relationship('Recipe', backref='ingredients')
+
+class Step(db.Model):
+    step_id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.recipe_id'), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    order_number = db.Column(db.Integer, nullable=False)
+    recipe = db.relationship('Recipe', backref='steps')
+
+# Create the tables (run this once to create the tables)
+with app.app_context():
+    db.create_all()
