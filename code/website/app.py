3f437f997fa115103b4ef50e6f9b85352cc00288
Emir Abou Zaki <emirabouzaki@gmail.com>
2023-11-21 12:35:54 +0000

    fixed database commands in flask app

--- code/website/app.py
+++ code/website/app.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, request, db
+from flask import Flask, render_template, request, g
 import sqlite3
 
 app = Flask(__name__, static_url_path='/static')
 app.config['DATABASE'] = 'cookbook.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(app.config['DATABASE'])
+    return db
 
 @app.route('/')
 def index():
@@ -11,7 +17,7 @@ def index():
 @app.route('/recipe/<recipe_name>')
 def serveRecipe(recipe_name):
     
-    return render_Template("build_recipe.html")
+    return render_template("build_recipe.html")
 
 
 @app.route('/chicken-korma')
@@ -28,6 +34,7 @@ def salmonFishcakes():
 
 @app.route('/publish', methods=['POST'])
 def publish():
+    cur = get_db().cursor()
     authorName = request.form['authorname']
     email = request.form['email']
     recipeName = request.form['recipename']
@@ -40,34 +47,40 @@ def publish():
     cuisine = request.form['cuisine']
 
     #Populate Cuisines Table
-    cuisineList = ('''SELECT name FROM Cuisines;''').fetchall() #Creates a list of all cuisines already in the database
+    cuisineList = cur.execute('''SELECT name FROM Cuisines;''').fetchall() #Creates a list of all cuisines already in the database
     if cuisine not in cuisineList:
-        db.execute('''INSERT INTO Cuisines (cuisine_id, cuisine_name) VALUES (1, ?);''', (cuisine))
+        cur.execute('''INSERT INTO Cuisines (cuisine_name) VALUES (?);''', (cuisine))
 
-    cuisineID = db.execute('''SELECT cuisine_id FROM Cuisines WHERE cuisine_name = ?;''', (cuisine)).fetchone()[0]
+    cuisineID = cur.execute('''SELECT cuisine_id FROM Cuisines WHERE cuisine_name = ?;''', (cuisine)).fetchone()[0]
 
     #Populate Authors Table
-    db.execute('''INSERT INTO Authors (author_id, name, email, bio) 
-                VALUES (1, ?, ?, '');''', (authorName, email))
+    cur.execute('''INSERT INTO Authors (name, email) 
+                VALUES (?, ?);''', (authorName, email))
 
-    authorID = db.execute('''SELECT author_id FROM Authors WHERE name=?;''', (authorName)).fetchone()[0]
+    authorID = cur.execute('''SELECT author_id FROM Authors WHERE name=?;''', (authorName)).fetchone()[0]
 
     #Populate Recipes Table
-    db.execute('''INSERT INTO Recipes (recipe_id, name, description, cuisine_id, serves, image, cooking_time, author_id) 
-                VALUES (1, ?, ?, ?, ?, ?, ?, ?);''', (recipeName, recipeDesc,cuisineID, serves, image, time, authorID))
+    cur.execute('''INSERT INTO Recipes (name, description, cuisine_id, serves, image, cooking_time, author_id) 
+                VALUES (?, ?, ?, ?, ?, ?, ?);''', (recipeName, recipeDesc,cuisineID, serves, image, time, authorID))
 
-    recipeID = db.execute('''SELECT recipe_id FROM Recipes WHERE name=?;''', (recipeName)).fetchone()[0]
+    recipeID =  cur.execute('''SELECT recipe_id FROM Recipes WHERE name=?;''', (recipeName)).fetchone()[0]
 
     #Populate Ingredients Table
     for ingredient in ingredients:
-        db.execute('''INSERT INTO Ingredients (ingredient_id, recipe_id, name, quantity) 
-                    VALUES (1, ?, ?, ?);''', (recipeID, ingredient[0], ingredient[1]))
+        cur.execute('''INSERT INTO Ingredients (recipe_id, name, quantity) 
+                    VALUES (?, ?, ?);''', (recipeID, ingredient[0], ingredient[1]))
 
     #Populate Steps Table
     i=1
     for step in steps:
-        db.execute('''INSERT INTO Steps (step_id, recipe_id, description, order_number) 
-                    VALUES (1, ?, ?, ?);''', (recipeID,step, i))
+        cur.execute('''INSERT INTO Steps (recipe_id, description, order_number) 
+                    VALUES (?, ?, ?);''', (recipeID,step, i))
         i+=1
 
     return 'Recipe inserted successfully'
+
+@app.teardown_appcontext
+def close_db(error):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
