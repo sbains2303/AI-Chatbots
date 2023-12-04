e5e151804ed0d88018fd5f7e87f374c6f47e131a
Saihajpreet Bains <ia22870@bristol.ac.uk>
2023-12-04 18:53:29 +0000

    Moved carousel
    Placed carousel into home page

--- code/website/app.py
+++ code/website/app.py
@@ -13,6 +13,7 @@ db = SQLAlchemy(app)
 def home():
     return render_template("layout.html")
 
+
 with open("templates/scrambled-eggs-content.yaml", "r") as file:
     scrambled_eggs_content = yaml.safe_load(file)
 
@@ -23,7 +24,7 @@ def scrambledEggs():
 with open("templates/chicken-korma.yaml", "r") as file:
     chicken_korma_content = yaml.safe_load(file)
 
- @app.route('/chicken-korma')
+@app.route('/chicken-korma')
 def chickenKorma():
     return render_template("recipes.html", **chicken_korma_content)
 
@@ -34,9 +35,9 @@ with open("templates/salmon-fishcakes-content.yaml", "r") as file:
 def salmonFishcakes():
     return render_template("recipes.html", **salmon_fishcakes_content)
 
-@app.route('/new-recipe')
-def newRecipe():
-    return render_template("new-recipe.html")
+# @app.route('/new-recipe')
+# def newRecipe():
+#     return render_template("new-recipe.html")
   
 @app.route('/carousel_test')
 def carousel_test():
