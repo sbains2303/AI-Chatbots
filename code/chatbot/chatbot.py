778abcf670aefa7e36945f83479872aa6af7d5c6
SebWhipps-Br <123549554+SebWhipps-Br@users.noreply.github.com>
2023-12-08 11:54:21 +0000

    Created python command line chatbot

---
+++ code/chatbot/chatbot.py
@@ -0,0 +1,40 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+
+class Chatbot:
+    def __init__(self):
+        print("New Chatbot")
+        self.messages = [{"role": "system", "content": "You are a helpful assistant."}, ]
+
+    def inputPrompt(self):
+        pass
+
+    def flow(self):
+        message = input()
+
+        self.messages.append({"role": "user", "content": message})
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=self.messages
+        )
+        r = completion.choices[0].message.content
+        print(f"Bot: {r}")
+        self.messages.append({"role": "user", "content": r})
+
+    def loop(self):
+        while True:
+            self.flow()
+
+
+if __name__ == "__main__":
+    print("Start chatting with the bot (type 'quit' to stop)!")
+    chatbot = Chatbot()
+    chatbot.loop()
